545e5e0aae897f2d5a76224975acb4f458b0eea9
akultikkas11 <akultikkas11@gmail.com>
2025-06-28 18:03:41 +0530

    Changed the base url

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ import { store } from './redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <Router basename="/Spotiy_2.0_Clone_assignment">
         <App />
       </Router>
     </Provider>
